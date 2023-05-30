4810277d7056b5d40ee3ee493d2ca1a8de664843
Александр Таркин <fk.theer@gmail.com>
2023-05-30 17:48:17 +0400

    Work2

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+        public static void main(String[]args){
+                double max_value;
+                max_value = 123456789.123456789;
+                System.out.println(max_value);
+                byte min_value;
+                min_value = 127;
+                System.out.println(min_value);}
+}
+
+
